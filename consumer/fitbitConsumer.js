8da417c8d40270fe5c1b8b5342f08006d23d9208
patrick9958 <patrick.neggie@gmail.com>
2022-07-27 19:47:36 -0400

    fitbitConsumer + fitbitProducer function stubs

---
+++ consumer/fitbitConsumer.js
@@ -0,0 +1,22 @@
+import Kafka from "node-rdkafka";
+import fitbitType from "../eventType.js";
+
+var consumer = new Kafka.KafkaConsumer(
+	{
+		"group.id": "kafka",
+		"metadata.broker.list": "localhost:9092",
+	},
+	{}
+);
+
+consumer.connect();
+
+consumer
+	.on("ready", () => {
+		console.log("consumer ready..");
+		consumer.subscribe(["fitbitTest"]);
+		consumer.consume();
+	})
+	.on("data", function (data) {
+		console.log(`received message: ${fitbitType.fromBuffer(data.value)}`);
+	});
